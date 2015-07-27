998a4842989cc1437c31b0610efb3b453c638728
kongbaguni <kongbaguni@gmail.com>
2015-07-27 14:29:37 +0900

    아이템 스트링 값에 따라 색깔 다르게 표현하기.

--- Classes/Item.cpp
+++ Classes/Item.cpp
@@ -43,7 +43,19 @@ bool Item::init(std::string strValue)
     auto label = Label::createWithBMFont("fonts/bmfont.fnt",strValue);
     label->setColor(Color3B::BLUE);
     label->setPosition(getContentSize()*0.5f);
-    label->setScale(0.5f);
+    label->setScale(1.6f/strValue.size()+0.2f);
+    if (strValue.compare("KC_TRNS")==0)
+    {
+        label->setColor(Color3B::WHITE);
+    }
+    else if (strValue.compare("FUNC(2)")==0 || strValue.compare("FUNC(1)")==0)
+    {
+        label->setColor(Color3B::RED);
+    }
+    else if (strValue.size()<=4)
+    {
+        label->setColor(Color3B::YELLOW);
+    }
     addChild(label);
     
     return true;
